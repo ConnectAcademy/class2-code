f56178df5f47cd0ef3b3bae96745e89d0ad825bc
ConnectAcademy <admen.sherifoski@ricci.mk>
2022-06-14 20:25:58 +0200

    React state classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { render } from "@testing-library/react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <h1>Hello World!</h1>;
+  const [count, setCount] = useState(0);
+  const [savedEntries, setSavedEntries] = useState([]);
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setSavedEntries((prev) => [...prev, count])}>
+        Save
+      </button>
+      {savedEntries.length >= 1 && (
+        <ul>
+          {savedEntries.map((elem) => (
+            <li>{elem}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default App;
