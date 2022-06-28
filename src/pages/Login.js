2498a4456c5c18f503d1cb2517c7e982182e9faf
ConnectAcademy <admen.sherifoski@ricci.mk>
2022-06-28 20:20:49 +0200

    trying to consume context

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+//importirajte  Context
+// importirate hook za da koristime kontext
+
+const Login = () => {
+  return (
+    <form>
+      <input placeholder="Enter your email" />
+      <button>Log In</button>
+    </form>
+  );
+};
+
+export default Login;
